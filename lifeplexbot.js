93113c871e7ceece8dd9ee273da4fd1dc0c8f2bc
badhatalaf <113542134+badhatalaf@users.noreply.github.com>
2023-06-23 16:37:22 +0530

    Update lifeplexbot.js

--- lifeplexbot.js
+++ lifeplexbot.js
@@ -1,11 +1,24 @@
 const mineflayer = require('mineflayer')
-const bot = mineflayer.createBot({
-    host: 'lifeplex.aternos.me',
-    username: 'botty',
-    version: '1.19.1'
-})
+const botCode = (bot) => {
+    bot.once('spawn', () => {
+        console.log(bot.username + ' is on!!')
+        bot.chat('Hiya!')
+    })
+}
 
-bot.once('spawn', () => {
-    console.log(bot.username + ' is on!!')
-    bot.chat('Hiya!')
-})
+const connectBot = (username) => {
+    const bot = mineflayer.createBot({
+        host: 'lifeplex.aternos.me',
+        username: username,
+        version: '1.18.2'
+    })
+
+    bot.once('end', () => {
+        console.log(`Bot ${username} has been disconnected. Reconnecting in 5 seconds...`)
+        setTimeout(() => connectBot(username), 5000)
+    })
+
+    botCode(bot)
+}
+
+connectBot('botty')
